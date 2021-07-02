6ecc486d17cee7ec502ad82544459910bb6f7c58
raziman <raziman.terra@gmail.com>
2021-07-02 16:30:56 +0800

    update test sample

--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,16 @@
 import canvacord from "../index";
 
 const img = "https://cdn.discordapp.com/embed/avatars/0.png";
+const bg = "https://cdn.discordapp.com/attachments/852546444086214676/860430326946070558/image0.jpg"
 
 const rank = new canvacord.Rank()
     .setAvatar(img)
     .setCurrentXP(50)
     .setRequiredXP(360)
+    .setBackground("IMAGE", bg)
     .setStatus("dnd")
-    .setProgressBar("#FFFFFF", "COLOR", false)
+    .setOverlay("#000")
+    .setProgressBar("#ff0800", "COLOR", false)
     .setUsername("Snowflake Game Master Foul Master")
     .setBronze(5)
     .setSilver(3)
